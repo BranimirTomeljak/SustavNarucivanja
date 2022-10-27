8896102eda27d4a6b2d3258c2b1eb90727d398b0
Marin Teskera <marin.teskera@infinum.com>
2022-10-27 17:00:28 +0200

    moced header and footer to components

--- sustav-narucivanja/src/app/app-routing.module.ts
+++ sustav-narucivanja/src/app/app-routing.module.ts
@@ -16,6 +16,7 @@ const routes: Routes = [
       { path: 'register', component: RegisterComponent },
     ],
   },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
---
+++ sustav-narucivanja/src/app/components/main-navigation/main-navigation.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-navigation',
+  templateUrl: './main-navigation.component.html',
+  styleUrls: ['./main-navigation.component.scss'],
+})
+export class MainNavigationComponent {}
