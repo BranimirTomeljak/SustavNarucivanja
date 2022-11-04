8e842355b554c0e8b69cc08e17da6b20040cf86f
Marin Teskera <marin.teskera@infinum.com>
2022-11-04 20:17:18 +0100

    initial register

--- sustav-narucivanja/src/app/pages/register/register.component.ts
+++ sustav-narucivanja/src/app/pages/register/register.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  public form: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    repeatedPassword: new FormControl('', [Validators.required]),
+    name: new FormControl('', [Validators.required]),
+    surname: new FormControl('', [Validators.required]),
+    sex: new FormControl('', [Validators.required]),
+    phoneNumber: new FormControl('', [Validators.required]),
+  });
+
+  public onFormSubmit(): void {}
+}
--- sustav-narucivanja/src/app/pages/register/register.module.ts
+++ sustav-narucivanja/src/app/pages/register/register.module.ts
@@ -1,10 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
+import { MainFooterModule } from 'src/app/components/main-footer/main-footer.module';
+import { MainNavigationModule } from 'src/app/components/main-navigation/main-navigation.module';
 import { RegisterComponent } from './register.component';
 
 @NgModule({
   declarations: [RegisterComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MainNavigationModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MainFooterModule,
+    RouterModule,
+  ],
   exports: [RegisterComponent],
 })
 export class RegisterModule {}
