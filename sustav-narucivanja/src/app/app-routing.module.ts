eceaf4b4dc825e3e741ee1c1e7abc08086d160b3
Marin Teskera <marin.teskera@infinum.com>
2022-11-05 12:34:45 +0100

    routing from admin to create pages

--- sustav-narucivanja/src/app/app-routing.module.ts
+++ sustav-narucivanja/src/app/app-routing.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './pages/admin/admin.component';
 import { AdminModule } from './pages/admin/admin.module';
+import { DoctorComponent } from './pages/admin/create/doctor/doctor.component';
+import { DoctorModule } from './pages/admin/create/doctor/doctor.module';
+import { TeamComponent } from './pages/admin/create/team/team.component';
+import { TeamModule } from './pages/admin/create/team/team.module';
+import { TechComponent } from './pages/admin/create/tech/tech.component';
+import { TechModule } from './pages/admin/create/tech/tech.module';
 import { HomeComponent } from './pages/home/home.component';
 import { HomeModule } from './pages/home/home.module';
 import { LoginComponent } from './pages/login/login.component';
@@ -15,6 +21,14 @@ const routes: Routes = [
     children: [
       { path: '', component: HomeComponent },
       { path: 'admin', component: AdminComponent },
+    ],
+  },
+  {
+    path: 'create',
+    children: [
+      { path: 'doctor', component: DoctorComponent },
+      { path: 'tech', component: TechComponent },
+      { path: 'team', component: TeamComponent },
     ],
   },
   {
@@ -34,6 +48,9 @@ const routes: Routes = [
     LoginModule,
     RegisterModule,
     AdminModule,
+    DoctorModule,
+    TechModule,
+    TeamModule,
   ],
   exports: [RouterModule],
 })
--- sustav-narucivanja/src/app/pages/admin/admin.module.ts
+++ sustav-narucivanja/src/app/pages/admin/admin.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 import { MainNavigationModule } from 'src/app/components/main-navigation/main-navigation.module';
 import { AdminComponent } from './admin.component';
 
 @NgModule({
   declarations: [AdminComponent],
-  imports: [CommonModule, MainNavigationModule, MatButtonModule],
+  imports: [CommonModule, MainNavigationModule, MatButtonModule, RouterModule],
   exports: [AdminComponent],
 })
 export class AdminModule {}
