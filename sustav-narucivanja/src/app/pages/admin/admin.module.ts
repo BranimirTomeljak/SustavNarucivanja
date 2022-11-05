cdc7fda0d246fb1baa4b1964531876d9805ed908
Marin Teskera <marin.teskera@infinum.com>
2022-11-05 12:00:52 +0100

    main navigation added to admin homepage

--- sustav-narucivanja/src/app/pages/admin/admin.module.ts
+++ sustav-narucivanja/src/app/pages/admin/admin.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MainNavigationModule } from 'src/app/components/main-navigation/main-navigation.module';
 import { AdminComponent } from './admin.component';
 
 @NgModule({
   declarations: [AdminComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MainNavigationModule],
   exports: [AdminComponent],
 })
 export class AdminModule {}
