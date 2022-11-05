b7f9546ae3e1b48e1fb7a4cb4e6da67678f58077
Marin Teskera <marin.teskera@infinum.com>
2022-11-05 12:13:11 +0100

    removed footer form login and register

--- sustav-narucivanja/src/app/pages/login/login.module.ts
+++ sustav-narucivanja/src/app/pages/login/login.module.ts
@@ -19,7 +19,6 @@ import { LoginComponent } from './login.component';
     ReactiveFormsModule,
     MatFormFieldModule,
     MainNavigationModule,
-    MainFooterModule,
     RouterModule,
   ],
   exports: [LoginComponent],
