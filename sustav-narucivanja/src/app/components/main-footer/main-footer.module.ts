5d277b25fb3a1683d4296700bbe4218fef9cf8f4
brunoracki <bruno.racki@fer.hr>
2022-11-07 09:03:49 +0100

    bug fixes, homepage almost done, added a lot of design

--- sustav-narucivanja/src/app/components/main-footer/main-footer.module.ts
+++ sustav-narucivanja/src/app/components/main-footer/main-footer.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainFooterComponent } from './main-footer.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [MainFooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   exports: [MainFooterComponent],
 })
 export class MainFooterModule {}
