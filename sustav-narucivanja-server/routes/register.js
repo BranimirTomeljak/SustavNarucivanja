64a937efa92109a6188a1d3336cf18d8bdec42a5
BranimirTomeljak <mtomeljak@gmail.com>
2022-11-09 19:10:55 +0100

    Fixed sending req.body in POST

--- sustav-narucivanja-server/routes/register.js
+++ sustav-narucivanja-server/routes/register.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   let jsonResponse = {
     "email": req.email,
     "password": "tomeljak",
@@ -12,8 +12,22 @@ router.get('/', function(req, res, next) {
     "sex": "ante",
     "phoneNumber": "ante"
   }
-  console.log(req.headers); //za dobit email sa frontenda
+  console.log(req.body); //za dobit email sa frontenda
   res.json(req.body);
+});*/
+
+router.post('/', function(req, res, next) {
+  const data = {
+    'email': req.body.email,
+    'password': req.body.password,
+    'repeatedPassword': req.body.repeatedPassword,
+    'name': req.body.name,
+    'surname': req.body.surname,
+    'sex': req.body.sex,
+    'phoneNumber': req.body.phoneNumber
+  }
+  res.json(data);
+  console.log(req.body);
 });
 
 module.exports = router;
