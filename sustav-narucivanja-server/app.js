9ff009820f82db655e8cd61d93a17d8768be6359
tensorway <darijan.gudelj@gmail.com>
2022-11-10 17:57:18 +0100

    post and get appointments

--- sustav-narucivanja-server/app.js
+++ sustav-narucivanja-server/app.js
@@ -10,6 +10,7 @@ var patientRouter = require('./routes/patient');
 var createRouter = require('./routes/create');
 var loginRouter = require('./routes/login');
 var registerRouter = require('./routes/register');
+var appointmentRouter = require('./routes/appointment');
 
 var app = express();
 
@@ -24,24 +25,25 @@ app.use('/patient', patientRouter);
 app.use('/create', createRouter);
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
+app.use('/appointment', appointmentRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
 
-console.log("starting...")
+console.log("started on port 3000")
 app.listen(3000)
