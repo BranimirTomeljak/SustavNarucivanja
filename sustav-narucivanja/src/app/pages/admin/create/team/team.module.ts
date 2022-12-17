922c1b74368bef059d3d9c5a71b410c502de85a5
Luka <luka.slugecic@gmail.com>
2022-12-17 21:59:51 +0100

    napravljena forma za stavaranje tima

--- sustav-narucivanja/src/app/pages/admin/create/team/team.module.ts
+++ sustav-narucivanja/src/app/pages/admin/create/team/team.module.ts
@@ -2,10 +2,33 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TeamComponent } from './team.component';
 import { TeamFormModule } from 'src/app/components/team-form/team-form.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthNavigationModule } from 'src/app/components/auth-navigation/auth-navigation.module';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MainNavigationModule } from 'src/app/components/main-navigation/main-navigation.module';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [TeamComponent],
-  imports: [CommonModule, TeamFormModule],
+  imports: [
+    CommonModule,
+    TeamFormModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MainNavigationModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    RouterModule,
+    MatSelectModule,
+    MatSnackBarModule
+    ],
   exports: [TeamComponent],
 })
-export class TeamModule {}
+export class TeamModule {
+}
