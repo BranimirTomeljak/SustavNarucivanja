689bd08460f572d460246494a7fbfab08059c966
Marin Teskera <marin.teskera@infinum.com>
2022-12-30 13:00:04 +0100

    all teams view

--- sustav-narucivanja/src/app/pages/admin/all-teams/all-teams.module.ts
+++ sustav-narucivanja/src/app/pages/admin/all-teams/all-teams.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AllTeamsComponent } from './all-teams.component';
+import { MainNavigationModule } from 'src/app/components/main-navigation/main-navigation.module';
 
 @NgModule({
   declarations: [AllTeamsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MainNavigationModule],
   exports: [AllTeamsComponent],
 })
 export class AllTeamsModule {}
