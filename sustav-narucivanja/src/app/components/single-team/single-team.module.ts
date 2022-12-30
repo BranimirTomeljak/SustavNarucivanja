b2f76d7ade78b839282236e48a8a4af65b008c5f
Marin Teskera <marin.teskera@infinum.com>
2022-12-30 13:48:10 +0100

    add delete button

--- sustav-narucivanja/src/app/components/single-team/single-team.module.ts
+++ sustav-narucivanja/src/app/components/single-team/single-team.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SingleTeamComponent } from './single-team.component';
 import { MainNavigationModule } from '../main-navigation/main-navigation.module';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [SingleTeamComponent],
-  imports: [CommonModule, MainNavigationModule],
+  imports: [CommonModule, MainNavigationModule, MatButtonModule],
   exports: [SingleTeamComponent],
 })
 export class SingleTeamModule {}
