0329581285b84cff26b5ccb98f0e97765ac028dd
Marin Teskera <marin.teskera@infinum.com>
2022-12-30 12:53:15 +0100

    create all teams component

---
+++ sustav-narucivanja/src/app/pages/admin/all-teams/all-teams.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AllTeamsComponent } from './all-teams.component';
+
+@NgModule({
+  declarations: [AllTeamsComponent],
+  imports: [CommonModule],
+  exports: [AllTeamsComponent],
+})
+export class AllTeamsModule {}
