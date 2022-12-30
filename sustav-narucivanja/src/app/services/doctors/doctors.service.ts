0dc7e3325a124fac8e0440a868e93adda984d13e
Marin Teskera <marin.teskera@infinum.com>
2022-12-30 09:09:39 +0100

    create teams page

--- sustav-narucivanja/src/app/services/doctors/doctors.service.ts
+++ sustav-narucivanja/src/app/services/doctors/doctors.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { IObservable } from 'mobx';
-import { Observable, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ITeamCreateData } from 'src/app/interfaces/team-create-data';
 
 @Injectable({
