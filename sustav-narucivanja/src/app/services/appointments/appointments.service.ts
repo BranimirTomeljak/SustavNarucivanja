68b6c8ff93175246d71425095877f200f1ea0e98
Luka <luka.slugecic@gmail.com>
2023-01-02 11:54:39 +0100

    dodan placeholder za appointment service

---
+++ sustav-narucivanja/src/app/services/appointments/appointments.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+//import { IObservable } from 'mobx';
+import { Observable, tap, BehaviorSubject } from 'rxjs';
+import { IAppointmentData } from 'src/app/interfaces/appointment-data';
+import { IRangeData } from 'src/app/interfaces/range-date';
+import { IRegisterData } from 'src/app/interfaces/register-data';
+import { IUser } from 'src/app/interfaces/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppointmentsService {
+  id = localStorage.getItem("id");
+
+  constructor(private http: HttpClient) {}
+  
+  public getAllApointments(): Observable<Array<any>> {
+    return this.http.get<Array<any>>('/api/appointment?role=patient&id=1001');
+  }
+
+  public addAppointment(data : IAppointmentData) {
+    return this.http.post('/api/appointment/add', data);
+  }
+
+  public addRangeAppointment(data : IRangeData) {
+    return this.http.post('/api/appointment/add_range', data);
+  }
+
+
+// TO DO bot ce ih jos....
+
+
+
+}
