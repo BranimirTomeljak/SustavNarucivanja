ae699cc1e34dc110409db5f4fcf4c73195cf433b
Marin Teskera <marin.teskera@infinum.com>
2023-01-03 22:08:50 +0100

    refinement

--- sustav-narucivanja/src/app/pages/admin/team-view/team-view.component.ts
+++ sustav-narucivanja/src/app/pages/admin/team-view/team-view.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { ISingleTeam } from 'src/app/interfaces/single-team';
 import { DoctorsService } from 'src/app/services/doctors/doctors.service';
 
@@ -8,7 +9,8 @@ import { DoctorsService } from 'src/app/services/doctors/doctors.service';
   templateUrl: './team-view.component.html',
   styleUrls: ['./team-view.component.scss'],
 })
-export class TeamViewComponent {
+export class TeamViewComponent implements OnDestroy {
+  private readonly subscription = new Subscription();
   public teamId?: number;
   public data?: ISingleTeam;
 
@@ -17,19 +19,25 @@ export class TeamViewComponent {
     private readonly doctorsService: DoctorsService,
     private readonly router: Router
   ) {
-    this.route.params.subscribe((params) => {
+    const routeSub = this.route.params.subscribe((params) => {
       this.teamId = params['id'];
-      this.doctorsService
+      const docSub = this.doctorsService
         .getTeamById(this.teamId as number)
         .subscribe((result) => {
           this.data = result;
         });
+      this.subscription.add(docSub);
     });
+    this.subscription.add(routeSub);
   }
 
   public onDeleteClick(): void {
     this.doctorsService.deleteTeam(this.teamId as number).subscribe(() => {
       this.router.navigate(['/admin/teams']);
     });
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
