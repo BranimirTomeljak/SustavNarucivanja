a72527847f7e3f20b883638c762fc40a0c6258ac
Marin Teskera <marin.teskera@infinum.com>
2023-01-12 21:55:54 +0100

    nurse working hours button

--- sustav-narucivanja/src/app/pages/nurse-page/nurse-working-hours/nurse-working-hours.module.ts
+++ sustav-narucivanja/src/app/pages/nurse-page/nurse-working-hours/nurse-working-hours.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NurseWorkingHoursComponent } from './nurse-working-hours.component';
-import { MainNavigationModule } from 'src/app/components/main-navigation/main-navigation.module';
-import { MainFooterModule } from 'src/app/components/main-footer/main-footer.module';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { AuthNavigationModule } from 'src/app/components/auth-navigation/auth-navigation.module';
-import { MatInputModule } from '@angular/material/input';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterModule } from '@angular/router';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
+import { RouterModule } from '@angular/router';
+import { AuthNavigationModule } from 'src/app/components/auth-navigation/auth-navigation.module';
+import { MainFooterModule } from 'src/app/components/main-footer/main-footer.module';
+import { MainNavigationModule } from 'src/app/components/main-navigation/main-navigation.module';
+import { NurseWorkingHoursComponent } from './nurse-working-hours.component';
 
 @NgModule({
   declarations: [NurseWorkingHoursComponent],
